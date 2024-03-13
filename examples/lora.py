bc5ecf128710571a310ec5f348bfc1a9446d7dcc
xhedit <71542719+xhedit@users.noreply.github.com>
2024-03-13 08:20:18 -0400

    will it blend?

--- examples/lora.py
+++ examples/lora.py
@@ -2,7 +2,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from exllamav2 import (
+from exl2conv import (
     ExLlamaV2,
     ExLlamaV2Config,
     ExLlamaV2Cache,
@@ -10,7 +10,7 @@ from exllamav2 import (
     ExLlamaV2Lora,
 )
 
-from exllamav2.generator import (
+from exl2conv.generator import (
     ExLlamaV2BaseGenerator,
     ExLlamaV2StreamingGenerator,
     ExLlamaV2Sampler
