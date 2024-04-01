0f5a59727f89b03b0ebbe422108acf468b88e516
xhedit <71542719+xhedit@users.noreply.github.com>
2024-03-31 21:50:30 -0400

    fix merge

--- exl2conv/generator/__init__.py
+++ exl2conv/generator/__init__.py
@@ -3,7 +3,7 @@ from exl2conv.version import __version__
 from exl2conv.generator.sampler import ExLlamaV2Sampler
 from exl2conv.generator.base import ExLlamaV2BaseGenerator
 from exl2conv.generator.streaming import ExLlamaV2StreamingGenerator
-
+from exl2conv.generator.hooks import ExLlamaV2PostSamplingHook, ExLlamaV2PostSamplingResult
 
 
 
