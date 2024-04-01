9dc0c7083d67f2d15b849ca491125d8d75a50f4b
xhedit <71542719+xhedit@users.noreply.github.com>
2024-04-01 01:56:26 +0000

    fix merge

--- exl2conv/tokenizer/__init__.py
+++ exl2conv/tokenizer/__init__.py
@@ -1,5 +1,5 @@
-from exllamav2.version import __version__
+from exl2conv.version import __version__
 
-from exllamav2.tokenizer.base import ExLlamaV2TokenizerBase
-from exllamav2.tokenizer.spm import ExLlamaV2TokenizerSPM
-from exllamav2.tokenizer.hf import ExLlamaV2TokenizerHF
+from exl2conv.tokenizer.base import ExLlamaV2TokenizerBase
+from exl2conv.tokenizer.spm import ExLlamaV2TokenizerSPM
+from exl2conv.tokenizer.hf import ExLlamaV2TokenizerHF
